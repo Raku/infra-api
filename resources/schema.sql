aef78f68b1975e7225d7bde3c75467a2b5bc2e8b
Andinus <andinus@nand.sh>
2023-05-18 09:20:39 +0530

    Update schema.sql

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,11 +1,28 @@
 CREATE TYPE task_status AS ENUM ('todo', 'in-progress', 'done', 'failed');
+CREATE TYPE task_type AS ENUM ('raku-doc-push');
 
 CREATE TABLE IF NOT EXISTS task(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    created TIMESTAMP WITH TIME ZONE DEFAULT now()
-    status task_status NOT NULL DEFAULT 'todo',
+    created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+
+    started TIMESTAMP WITH TIME ZONE,
     completed TIMESTAMP WITH TIME ZONE,
-    detail JSONB NOT NULL
+
+    status task_status NOT NULL DEFAULT 'todo',
+    type task_type NOT NULL DEFAULT 'raku-doc-push',
+
+    detail JSONB NOT NULL,
+    pid INTEGER,
+
+    priority SMALLINT DEFAULT 0,
+
+    -- if job is in progress then started column cannot be null.
+    CONSTRAINT task_status_in_progress_started_not_null_check
+        CHECK ( NOT (status = 'in-progress' AND (started IS NULL OR pid IS NULL)) ),
+
+    -- if job has completed then the column cannot be null.
+    CONSTRAINT task_status_done_failed_completed_not_null_check
+        CHECK ( NOT (status IN ('done', 'failed') AND completed IS NULL) )
 );
 
 -- capture_func executes a NOTIFY. it is used to listen on new row insertions.
@@ -13,15 +30,16 @@ CREATE FUNCTION capture_func()
 RETURNS trigger AS
 $$
 DECLARE
-  v_txt text;
+  v_txt TEXT;
 BEGIN
-  v_txt := format('new insert on %s, %s', TG_OP, NEW);
-  RAISE NOTICE '%', v_txt;
-    EXECUTE FORMAT('NOTIFY task_insert, ''%s''', v_txt);
+  v_txt := format('new %s: %s', TG_OP, NEW.id);
+  -- RAISE NOTICE '%', v_txt;
+  EXECUTE FORMAT('NOTIFY task_insert, ''%s''', v_txt);
   RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER task_new_row_trigger BEFORE INSERT
-       ON t_message
-       FOR EACH ROW EXECUTE PROCEDURE capture_func();
+CREATE TRIGGER task_new_row_trigger
+    BEFORE INSERT ON task
+    FOR EACH ROW
+    EXECUTE PROCEDURE capture_func();
