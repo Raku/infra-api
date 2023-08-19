edb9e8fbee5175d54301329ffc8a763daaf4a817
Andinus <andinus@nand.sh>
2023-08-19 22:26:16 +0530

    Move schema file
    I tried switching to DB::Migration::Simple but the module seems to be
    un-maintained and currently doesn't work, will require some patches to
    make it work, for now we can do things manually maybe.

--- resources/migrations.sql
+++ resources/migrations.sql
@@ -1,34 +1,34 @@
+-- 1 up
 CREATE TYPE task_status AS ENUM ('todo', 'in-progress', 'done', 'failed');
 CREATE TYPE task_type AS ENUM ('raku-doc-push');
 
--- task table holds all the tasks.
+/* task table holds all the tasks. */
 CREATE TABLE IF NOT EXISTS task(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    -- timestamps.
     created TIMESTAMP WITH TIME ZONE DEFAULT now(),
     started TIMESTAMP WITH TIME ZONE,
     completed TIMESTAMP WITH TIME ZONE,
 
-    -- task_type is for the taskrunner to differentiate tasks.
+    /* task_type is for the taskrunner to differentiate tasks. */
     status task_status NOT NULL DEFAULT 'todo',
     type task_type NOT NULL DEFAULT 'raku-doc-push',
 
     detail JSONB NOT NULL,
-    pid INTEGER, -- for taskrunner recordkeeping.
+    pid INTEGER, /* for taskrunner recordkeeping. */
 
     priority SMALLINT DEFAULT 0,
 
-    -- if job is in progress then started column cannot be null.
+    /* if job is in progress then started column cannot be null. */
     CONSTRAINT task_status_in_progress_started_not_null_check
         CHECK ( NOT (status = 'in-progress' AND (started IS NULL OR pid IS NULL)) ),
 
-    -- if job has completed then the column cannot be null.
+    /* if job has completed then the column cannot be null. */
     CONSTRAINT task_status_done_failed_completed_not_null_check
         CHECK ( NOT (status IN ('done', 'failed') AND completed IS NULL) )
 );
 
--- capture_func executes a NOTIFY. it is used to listen on new row insertions.
+/* capture_func executes a NOTIFY. it is used to listen on new row insertions. */
 CREATE FUNCTION capture_func()
 RETURNS trigger AS
 $$
@@ -36,7 +36,7 @@ DECLARE
   v_txt TEXT;
 BEGIN
   v_txt := format('new %s: %s', TG_OP, NEW.id);
-  -- RAISE NOTICE '%', v_txt;
+  /* RAISE NOTICE '%', v_txt; */
   EXECUTE FORMAT('NOTIFY task_insert, ''%s''', v_txt);
   RETURN NEW;
 END;
